e33df4334967510fe593532a1e10efa0d1d07f60
Nakul Tiwari <nakultiwari93@gmail.com>
2024-03-07 23:18:14 +0530

    First Commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import Hero from "./Hero";
+import Card from "./Card";
+import Popup from "./PopUp";
+export { Footer, Navbar, Logo, Menu, Card, Popup, Hero };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+//Internal Imports
+import { Navbar, Footer } from "../../components";
+import { CrowdFundingProvider } from "../../context/CrowdFunding";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CrowdFundingProvider>
+        <Navbar />
+        <Component {...pageProps} />;
+        <Footer />
+      </CrowdFundingProvider>
+    </>
+  );
 }
